e071c9addf970083c1db5e91aeceb0d6f1a8c0ef
103736914 <DavidAPetersen02@gmail.com>
2023-09-15 23:27:07 -0600

    Commented out Hello World code. Added grade lettering/reacting code.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What was your grade percentage? (Please round to the nearest whole number.) ");
+        string gradePercentageString = Console.ReadLine();
+        int gradePercentage = int.Parse(gradePercentageString);
+
+        string letter;
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your score: {letter}. ");
+        if (gradePercentage >= 70)
+        {
+            Console.Write("Congratulations on passing!");
+        }
+        else
+        {
+            Console.Write("You didn't pass. Better luck next time!");
+        }
     }
 }
+

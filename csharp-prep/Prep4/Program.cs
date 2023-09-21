cbb9f30ba2732a06ed8aab069559eced2637e02a
103736914 <DavidAPetersen02@gmail.com>
2023-09-20 23:28:18 -0600

    Commented out Hello World code; added number list tracker code.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+        
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userInput = 1;
+        int largestNumber = -999999;
+        float inputsTotal = 0;
+        List<int> userInputs = new List<int>();
+
+        while (userInput > 0)
+        {
+            Console.Write("Enter a number: ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                userInputs.Add(userInput);
+
+                inputsTotal += userInput;
+
+                if (userInput > largestNumber)
+                {
+                    largestNumber = userInput;
+                }
+            }
+
+        }
+
+        Console.WriteLine($"The sum is: {inputsTotal}");
+        Console.WriteLine($"The average is: {inputsTotal / userInputs.Count}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
+

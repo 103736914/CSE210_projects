6a115537488fe8e07a483832af09181b7b4107b6
103736914 <DavidAPetersen02@gmail.com>
2023-09-20 23:58:04 -0600

    Commented out Hello World code, added personal squared-number response code.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int inputInteger)
+    {
+        return inputInteger * inputInteger;
+    }
+
+    static void DisplayResult(string nameOfUser, int squaredNumber)
+    {
+        Console.Write($"{nameOfUser}, the square of your favorite number is {squaredNumber}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcome();
+
+        // DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
+
+        string nameOfUser = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(nameOfUser, squaredNumber);
+        
     }
 }
+
